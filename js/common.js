40013b76d94742b0cc20e83096823f1eba988bb7
Vitaliy <vetalsd2@gmail.com>
2017-05-07 20:02:26 +0300

    + second page

--- js/common.js
+++ js/common.js
@@ -26,6 +26,11 @@ $(document).ready(function() {
 		$('.menu ul li').removeClass('active');
 		$(this).addClass('active');
 	});
+	$('.news__selected--post .navigation span').click(function(){
+		$('.news__selected--post .navigation span').removeClass('active');
+		$(this).addClass('active');
+	});
+
 	//popup
 	$('.popup').magnificPopup({
 		type: 'inline'
@@ -35,58 +40,6 @@ $(document).ready(function() {
 	jQuery(function($){
 		$('.phone').mask('+38(099) 999-9999');
 	});
-
-
-
-	/*
-	//slick carousel
-	$('.photo_carousel').slick({
-		slidesToShow: 5,
-		slidesToScroll: 1,
-		asNavFor: '.photo_carousel_for',
-		focusOnSelect: true,
-		appendDots: '.review .nav_slider',
-		customPaging: function(slider, i) {
-			return '<div class="dot"></div>';
-		},
-		responsive: [
-			{
-			breakpoint: 992,
-			settings: {
-				slidesToShow: 3
-			}
-			}
-		]
-	});*/
-	/*
-	// photo magnific gallery
-	$('.photo_popup').magnificPopup({
-		type: 'image',
-		gallery: {
-			enabled: true
-		}
-	});
-	*/
-
-	/*
-	//video player + button
-	$('#mainVideo').click(function(){
-		if (this.paused) {
-			this.play();
-			$('#videoButton').fadeOut('fast');
-		} else {
-			$('#videoButton').fadeIn('fast');
-			this.pause();
-		}
-	});
-	$('#videoButton').on('click', function(event) {
-		event.preventDefault();
-		$('#videoButton').fadeOut('fast');
-		$('#mainVideo').get(0).play();
-	});
-	*/
-
-	
 	//validation
 	$('#feedbackForm').validate({
 		rules: {
@@ -218,9 +171,6 @@ $(document).ready(function() {
 			}
 		}
 	});
-
-
-
 });
 /*
 // preloader
